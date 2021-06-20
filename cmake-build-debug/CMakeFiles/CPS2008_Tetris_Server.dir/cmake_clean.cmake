56e0daaf307f33f517fd7c86b3a23c7e13f5f8d7
xmif1 <xandru.mifsud@gmail.com>
2021-06-20 09:06:25 +0200

    Added git ignore

---
+++ cmake-build-debug/CMakeFiles/CPS2008_Tetris_Server.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CPS2008_Tetris_Server.dir/server.c.o"
+  "CPS2008_Tetris_Server"
+  "CPS2008_Tetris_Server.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/CPS2008_Tetris_Server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
